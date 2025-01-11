3eb7339a8d8f5ab68a383c2bbd9b9b01121137eb
Alex <salmon_alexandre@outlook.fr>
2025-01-11 14:22:58 +0100

    [reform] remove css from the html

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -199,14 +199,14 @@
                         <div class="modal-body">
                             ${
                               navigation
-                                ? '<div class="mg-prev" style="cursor:pointer;position:absolute;top:50%;left:-15px;background:white;"><</div>'
-                                : '<span style="display:none;" />'
+                                ? '<div class="mg-prev" ><</div>'
+                                : '<span />'
                             }
                             <img class="lightboxImage img-fluid" alt="Contenu de l'image affichée dans la modale au clique"/>
                             ${
                               navigation
-                                ? '<div class="mg-next" style="cursor:pointer;position:absolute;top:50%;right:-15px;background:white;}">></div>'
-                                : '<span style="display:none;" />'
+                                ? '<div class="mg-next" >></div>'
+                                : '<span/>'
                             }
                         </div>
                     </div>
